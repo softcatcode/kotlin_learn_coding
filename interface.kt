e06ce326998d62896646090fa0be7a189de04b16
Daniil-coding <109597472+Daniil-coding@users.noreply.github.com>
2022-08-07 10:33:03 +0300

    Update interface.kt

--- interface.kt
+++ interface.kt
@@ -2,9 +2,9 @@ import java.lang.Math.sin
 
 interface GeomShape
 {
+    var name: String
     // functions may be implemented in interface
-    fun printProperties()
-    {
+    fun printProperties() {
         println("Properties:")
     }
     fun area(): Double
@@ -12,18 +12,23 @@ interface GeomShape
 
 class Parallelogram(x: Double, y: Double, a: Double): GeomShape
 {
+    // lateinit: initialized not imidiately but in init{}
+    override lateinit var name: String
+
     var x: Double
     var y: Double
     var a: Double
 
     init {
+        name = "parallelogram"
         this.x = x
         this.y = y
         this.a = a
     }
 
     override fun printProperties() {
-        super.printProperties()
+        // in this case may be just super.printProperties()
+        super<GeomShape>.printProperties()
         println("Oposite sides are parallel.")
         println("Oposite angles are equal.")
         println("Oposite sides are equal")
@@ -38,5 +43,5 @@ fun main()
 {
     var paral = Parallelogram(1.0, 2.0, 3.14159265358979 / 6)
     paral.printProperties()
-    print("Area is ${paral.area()}.")
+    print("Area of this ${paral.name} is ${paral.area()}.")
 }
